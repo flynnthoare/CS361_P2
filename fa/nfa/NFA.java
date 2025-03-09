692ce3d72d0f3024b76a4af8560a5805e2936bfe
NickDBortz <NicholasBortz@u.boisestate.edu>
2025-03-08 17:41:24 -0700

    Added implementing and extending classes

--- fa/nfa/NFA.java
+++ fa/nfa/NFA.java
@@ -1,5 +1,76 @@
 package fa.nfa;
 
-public class NFA {
-    
+import fa.State;
+
+public class NFA implements NFAInterface {
+
+    @java.lang.Override
+    public boolean addState(String name) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean setFinal(String name) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean setStart(String name) {
+        return false;
+    }
+
+    @java.lang.Override
+    public void addSigma(char symbol) {
+
+    }
+
+    @java.lang.Override
+    public boolean accepts(String s) {
+        return false;
+    }
+
+    @java.lang.Override
+    public Set<Character> getSigma() {
+        return null;
+    }
+
+    @java.lang.Override
+    public State getState(String name) {
+        return null;
+    }
+
+    @java.lang.Override
+    public boolean isFinal(String name) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean isStart(String name) {
+        return false;
+    }
+
+    @java.lang.Override
+    public Set<NFAState> getToState(NFAState from, char onSymb) {
+        return null;
+    }
+
+    @java.lang.Override
+    public Set<NFAState> eClosure(NFAState s) {
+        return null;
+    }
+
+    @java.lang.Override
+    public int maxCopies(String s) {
+        return 0;
+    }
+
+    @java.lang.Override
+    public boolean addTransition(String fromState, Set<String> toStates, char onSymb) {
+        return false;
+    }
+
+    @java.lang.Override
+    public boolean isDFA() {
+        return false;
+    }
 }
